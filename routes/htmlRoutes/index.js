c8782409d60c97b88989558b5bfae8964d93b31c
jack <biffisbuffm@gmail.com>
2020-08-06 14:13:00 -0500

    error still

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,15 @@
+
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+module.exports = router;
