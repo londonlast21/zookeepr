b7d8668ca04ad6d80763dbb0e8ec44b86555cf22
jack <biffisbuffm@gmail.com>
2020-08-05 23:17:51 -0500

    animals added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // [arse incoming JSON data
 app.use(express.json());
-
+app.use(express.static('public'));
 
 
 
@@ -84,6 +84,16 @@ app.get('/api/animals/:id', (req, res) => {
   res.send(404);
 }
 });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
 app.post('/api/animals', (req, res) => {
  // set id based on what the next index of the array will be
  req.body.id = animals.length.toString();
@@ -101,6 +111,7 @@ app.post('/api/animals', (req, res) => {
 
   res.json(animal);
 });
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
